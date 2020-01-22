fe7325a8b5b751fb15f5426daed52440f1bb2809
user <mohamed.reda.007007@gmail.com>
2020-01-22 18:55:02 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/reda/home_with_custom_painter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.reda.home_with_custom_painter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
