1a0b1d8a0dd4aff318bb569ac32d0641c50c0744
user <mohamed.reda.007007@gmail.com>
2020-01-23 17:03:23 +0200

    finished the circle and rect

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,11 +12,18 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: CustomPaint(
-          size: Size(double.infinity, double.infinity),
-          painter: Sky(),
-          child: Center(
-            child: Text('hello'),
+        body: Container(
+          color: Colors.blue,
+          child: SafeArea(
+            child: CustomPaint(
+              size: Size(double.infinity, double.infinity),
+              painter: Line(),
+//              BobRoss(),
+//                  Sky(),
+              child: Center(
+                child: Text('hello'),
+              ),
+            ),
           ),
         ),
       ),
@@ -27,52 +33,84 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Sky extends CustomPainter {
+//rectangle
+//class BobRoss extends CustomPainter {
+//  @override
+//  void paint(Canvas canvas, Size size) {
+//    Paint paint = Paint();
+//    paint.style = PaintingStyle.fill;
+//    paint.color = Colors.greenAccent;
+////    canvas.drawPath(, paint);
+//    canvas.drawRect(Rect.fromLTWH(0.0, 0.0, size.width, 50), paint);
+//  }
+//
+//  @override
+//  bool shouldRepaint(BobRoss oldDelegate) {
+//    return false;
+//  }
+//}
+class Line extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    var rect = Offset.zero & size;
-    var gradient = RadialGradient(
-      center: const Alignment(0.7, -0.6),
-      radius: 0.2,
-      colors: [const Color(0xFFFFFF00), const Color(0xFF0099FF)],
-      stops: [0.4, 1.0],
-    );
-    canvas.drawRect(
-      rect,
-      Paint()..shader = gradient.createShader(rect),
-    );
+    Paint paint = Paint();
+    paint.style = PaintingStyle.fill;
+    paint.color = Colors.greenAccent;
+//    canvas.drawPath(, paint);
+    canvas.drawLine(Offset(100, 90), Offset(0, 0), paint);
   }
 
   @override
-  SemanticsBuilderCallback get semanticsBuilder {
-    return (Size size) {
-      // Annotate a rectangle containing the picture of the sun
-      // with the label "Sun". When text to speech feature is enabled on the
-      // device, a user will be able to locate the sun on this picture by
-      // touch.
-      var rect = Offset.zero & size;
-      var width = size.shortestSide * 0.4;
-      rect = const Alignment(0.8, -0.9).inscribe(Size(width, width), rect);
-      return [
-        CustomPainterSemantics(
-          rect: rect,
-          properties: SemanticsProperties(
-            label: 'Sun',
-            textDirection: TextDirection.ltr,
-          ),
-        ),
-      ];
-    };
+  bool shouldRepaint(Line oldDelegate) {
+    return false;
   }
-
-  // Since this Sky painter has no fields, it always paints
-  // the same thing and semantics information is the same.
-  // Therefore we return false here. If we had fields (set
-  // from the constructor) then we would return true if any
-  // of them differed from the same fields on the oldDelegate.
-  @override
-  bool shouldRepaint(Sky oldDelegate) => false;
-
-  @override
-  bool shouldRebuildSemantics(Sky oldDelegate) => false;
 }
+
+//circle
+//class Sky extends CustomPainter {
+//  @override
+//  void paint(Canvas canvas, Size size) {
+//    //Returns a one in each bit position for
+//    // which the corresponding bits of both operands are ones.
+//    var rect = Offset.zero & size;
+//    var gradient = RadialGradient(
+//      center: const Alignment(0.7, -0.6),
+//      radius: 0.2,
+//      colors: [const Color(0xFFFFFF00), const Color(0xFF0099FF)],
+//      stops: [0.4, 1.0],
+//    );
+//    canvas.drawCircle(Offset(100, 200), 50, Paint());
+////    canvas.drawRect(
+////      rect,
+////      Paint()..shader = gradient.createShader(rect),
+////    );
+//  }
+//
+////  @override
+////  SemanticsBuilderCallback get semanticsBuilder {
+////    return (Size size) {
+////      var rect = Offset.zero & size;
+////      var width = size.shortestSide * 0.4;
+////      rect = const Alignment(0.8, -0.9).inscribe(Size(width, width), rect);
+////      return [
+////        CustomPainterSemantics(
+////          rect: rect,
+////          properties: SemanticsProperties(
+////            label: 'Sun',
+////            textDirection: TextDirection.ltr,
+////          ),
+////        ),
+////      ];
+////    };
+////  }
+//
+//  // Since this Sky painter has no fields, it always paints
+//  // the same thing and semantics information is the same.
+//  // Therefore we return false here. If we had fields (set
+//  // from the constructor) then we would return true if any
+//  // of them differed from the same fields on the oldDelegate.
+//  @override
+//  bool shouldRepaint(Sky oldDelegate) => false;
+//
+//  @override
+//  bool shouldRebuildSemantics(Sky oldDelegate) => false;
+//}
